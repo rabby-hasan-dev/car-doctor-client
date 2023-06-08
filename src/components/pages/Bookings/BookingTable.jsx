21e0f4dfa2a68370c2239f2438e6f88a0f3d2fd5
rabby6530 <you@example.com>
2023-06-08 12:52:57 +0600

    added delete method of my bookings

--- src/components/pages/Bookings/BookingTable.jsx
+++ src/components/pages/Bookings/BookingTable.jsx
@@ -1,29 +1,23 @@
 
 
-const BookingTable = ({ booking }) => {
+const BookingTable = ({ booking,handleDelete }) => {
     console.log(booking)
-    const { CustomerName, date, email, img, price, service_id, services } = booking;
-
+    const { _id, CustomerName, date, email, img, price, service_id, services } = booking;
 
 
 
     return (
         <tr>
             <th>
-                <label>
-                    <button className="btn btn-circle w-25">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                    </button>
-                </label>
+                <button onClick={()=>handleDelete(_id)} className="btn  btn-sm btn-circle w-25">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td>
-                <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src={img} alt="Avatar Tailwind CSS Component" />
-                        </div>
+                <div className="avatar">
+                    <div className="w-24 h-24 rounded-xl">
+                        <img src={img} />
                     </div>
-
                 </div>
             </td>
             <td>
--- src/components/pages/Bookings/Bookings.jsx
+++ src/components/pages/Bookings/Bookings.jsx
@@ -14,6 +14,26 @@ const Bookings = () => {
             .then(data => setBookings(data));
     }, [])
 
+
+    const handleDelete = (id) => {
+        const proceed = confirm('Are you sure Delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('Service delete successfully')
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        setBookings(remaining);
+                    }
+                })
+
+        }
+    }
+
     return (
         <div className="mt-12">
 
@@ -46,6 +66,8 @@ const Bookings = () => {
                             bookings.map((booking, index) => <BookingTable
                                 key={index + 1}
                                 booking={booking}
+                                handleDelete={handleDelete}
+
                             ></BookingTable>)
                         }
 
